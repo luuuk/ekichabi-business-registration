8aa6678555d9ddc9d3d20bdaada8bb7b91aab9cd
Luke LeVasseur <llevasseur@flexport.com>
2022-01-24 19:04:36 -0800

    logging

--- src/main/java/com/ekichabi_business_registration/api/BusinessController.java
+++ src/main/java/com/ekichabi_business_registration/api/BusinessController.java
@@ -2,6 +2,8 @@ package com.ekichabi_business_registration.api;
 
 import com.ekichabi_business_registration.db.entity.BusinessEntity;
 import com.ekichabi_business_registration.service.BusinessService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import java.util.List;
 @CrossOrigin(origins = {}) // TODO update with origins we want to allow requests from
 public class BusinessController {
     private final BusinessService service;
+    private final Logger logger = LoggerFactory.getLogger(BusinessController.class);
 
     public BusinessController(BusinessService service) {
         this.service = service;
@@ -22,6 +25,7 @@ public class BusinessController {
 
     @GetMapping
     public ResponseEntity<List<BusinessEntity>> findAll() {
+        logger.info("Calling FindAllBusinesses");
         List<BusinessEntity> businessEntities = service.findAllBusinesses();
         return ResponseEntity.ok().body(businessEntities);
     }
