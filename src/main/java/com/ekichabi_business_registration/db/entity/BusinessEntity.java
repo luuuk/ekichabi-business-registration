23132c3e2abe47cc8bb88a63e3fb392bd25465c3
Yihong Zhang <yz489@cs.washington.edu>
2022-01-25 23:51:58 -0800

    @RequiredArgsConstructor now working

--- src/main/java/com/ekichabi_business_registration/db/entity/BusinessEntity.java
+++ src/main/java/com/ekichabi_business_registration/db/entity/BusinessEntity.java
@@ -1,17 +1,9 @@
 package com.ekichabi_business_registration.db.entity;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.Hibernate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
