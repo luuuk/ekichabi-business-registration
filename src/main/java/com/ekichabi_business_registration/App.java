1cfcd2534c18dc783b9d8e3ec2f0ae9638f4c5b8
Luke LeVasseur <llevasseur@flexport.com>
2022-01-26 19:37:12 -0800

    remove unused import

--- src/main/java/com/ekichabi_business_registration/App.java
+++ src/main/java/com/ekichabi_business_registration/App.java
@@ -1,13 +1,10 @@
 package com.ekichabi_business_registration;
 
-import com.ekichabi_business_registration.db.entity.BusinessEntity;
 import com.ekichabi_business_registration.db.repository.BusinessRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class App {
