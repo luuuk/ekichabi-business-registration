451cbbd93f1f7ed5796c2ef61d60f4d38fc215b7
Luke LeVasseur <llevasseur@flexport.com>
2022-01-26 19:49:44 -0800

    Configure app for Heroku deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT --spring.datasource.url=${JDBC_DATABASE_URL}
+web: java -Dspring.profiles.active=prod -Dserver.port=$PORT -jar build/libs/springboot.heroku.app-0.0.1-SNAPSHOT.jar
--- src/main/java/com/ekichabi_business_registration/App.java
+++ src/main/java/com/ekichabi_business_registration/App.java
@@ -5,8 +5,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class App {
 
 	public static void main(String[] args) {
