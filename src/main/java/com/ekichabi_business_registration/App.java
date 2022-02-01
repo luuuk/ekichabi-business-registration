9d767d7b6399decb8e1e34db779ddbc492a1fa37
Luke LeVasseur <llevasseur@flexport.com>
2022-01-31 21:03:39 -0800

    test for pr

--- src/main/java/com/ekichabi_business_registration/App.java
+++ src/main/java/com/ekichabi_business_registration/App.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class App {
 
+    // Main app to run
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
