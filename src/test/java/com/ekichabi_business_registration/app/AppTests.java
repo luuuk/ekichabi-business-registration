04b8357dcb2347b774aa923488225dbc099c4593
Luke LeVasseur <llevasseur@flexport.com>
2022-01-31 21:17:36 -0800

    test for pr

--- src/test/java/com/ekichabi_business_registration/app/AppTests.java
+++ src/test/java/com/ekichabi_business_registration/app/AppTests.java
@@ -3,10 +3,13 @@ package com.ekichabi_business_registration.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+
 @SpringBootTest
 class AppTests {
 
 	@Test
+    @Transactional
 	void contextLoads() {
 	}
 
