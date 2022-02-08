fa45127df87fc530834fd1f213322b5ba1c12dc0
Luke LeVasseur <llevasseur@flexport.com>
2022-02-08 12:41:54 -0800

    Remove silly tests hehe

--- src/test/java/com/ekichabi_business_registration/app/service/BusinessServiceTests.java
+++ src/test/java/com/ekichabi_business_registration/app/service/BusinessServiceTests.java
@@ -1,38 +1,23 @@
 package com.ekichabi_business_registration.app.service;
 
 import com.ekichabi_business_registration.app.common.IntegrationTest;
-import com.ekichabi_business_registration.app.common.TestHelperService;
 import com.ekichabi_business_registration.db.entity.BusinessEntity;
 import com.ekichabi_business_registration.service.BusinessService;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @IntegrationTest
+// TODO complete test class
 public class BusinessServiceTests {
-    private static TestHelperService testHelperService;
 
     @Autowired
     private BusinessService businessService;
 
-    @BeforeAll
-    static void setUp() {
-        testHelperService.setUp();
-    }
-
-    @AfterAll
-    public static void tearDown() {
-        testHelperService.tearDown();
-    }
-
     @Test
     void findAllBusinesses_Success(){
      List<BusinessEntity> businessEntityList = businessService.findAllBusinesses();
-
-
-     //     assert businessEntityList.containsAll();
+     // TODO add entities to business repo and verify that they're returned here
     }
 }
