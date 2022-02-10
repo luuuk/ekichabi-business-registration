97b49142c2f067145187f6ca5c917dc9811c3642
Luke LeVasseur <llevasseur@flexport.com>
2022-02-09 16:04:50 -0800

    Deal with rebase

--- src/main/java/com/ekichabi_business_registration/db/entity/SubvillageEntity.java
+++ src/main/java/com/ekichabi_business_registration/db/entity/SubvillageEntity.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.Hibernate;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
