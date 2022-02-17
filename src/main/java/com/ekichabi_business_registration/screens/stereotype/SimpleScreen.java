9030643766d0b65631c1d445388a95381f4ab94b
Yihong Zhang <yz489@cs.washington.edu>
2022-02-17 10:57:13 -0800

    buffer is not initialized

--- src/main/java/com/ekichabi_business_registration/screens/stereotype/SimpleScreen.java
+++ src/main/java/com/ekichabi_business_registration/screens/stereotype/SimpleScreen.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleScreen extends Screen {
-    private StringBuilder buffer;
+    private StringBuilder buffer = new StringBuilder();
 
     public SimpleScreen(boolean shouldContinue) {
         super(shouldContinue);
