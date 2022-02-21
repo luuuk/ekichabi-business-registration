5447cf35bb053755dde96fc985efa70c2fb03161
Yihong Zhang <zyh3838438zyh@live.cn>
2022-02-20 16:33:22 -0800

    Make checkstyle happy

--- src/main/java/com/ekichabi_business_registration/db/entity/BusinessEntity.java
+++ src/main/java/com/ekichabi_business_registration/db/entity/BusinessEntity.java
@@ -12,7 +12,6 @@ import org.hibernate.Hibernate;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
