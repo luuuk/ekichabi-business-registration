2e70cf6705d08a7c842028c408f98060dbccf60d
Luke LeVasseur <luke.levasseur16@gmail.com>
2022-02-22 08:44:56 -0800

    Seed geo tables (#14)
    * Dont let hibernate directly update db schema
    
    * Create admin endpoints for seeding geo info
    
    * Temp
    
    Co-authored-by: Luke LeVasseur <llevasseur@flexport.com>

--- src/main/java/com/ekichabi_business_registration/controller/AdminController.java
+++ src/main/java/com/ekichabi_business_registration/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.ekichabi_business_registration.controller;
 
 import com.ekichabi_business_registration.service.CategoryService;
+import com.ekichabi_business_registration.service.GeoService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
     private static final String PASSWORD = "supersecretpassword";
     private final Logger logger = LoggerFactory.getLogger(BusinessController.class);
-    private final CategoryService service;
+    private final CategoryService categoryService;
+    private final GeoService geoService;
 
     /**
      * Creates categories based on the contents of census_full.csv
@@ -36,7 +38,7 @@ public class AdminController {
         }
         logger.info("Creating categories from Categories.txt");
         try {
-            int createdCount = service.createCategories();
+            int createdCount = categoryService.createCategories();
             return ResponseEntity.ok().body("Created " + createdCount + " new categories");
         } catch (Exception e) {
             return ResponseEntity.badRequest()
@@ -56,7 +58,7 @@ public class AdminController {
         }
         logger.info("Creating subcategories from Categories.txt");
         try {
-            int createdCount = service.createSubcategories();
+            int createdCount = categoryService.createSubcategories();
             return ResponseEntity.ok().body("Created " + createdCount + " new subcategories");
         } catch (Exception e) {
             return ResponseEntity.badRequest()
@@ -64,5 +66,63 @@ public class AdminController {
         }
     }
 
+    /**
+     * Creates districts based on the contents of census_full.csv
+     * in the resources/static directory
+     **/
+    @PostMapping("districts/{auth}")
+    public ResponseEntity<?> createDistricts(@PathVariable String auth) {
+        if (!auth.equals(PASSWORD)) {
+            return ResponseEntity.badRequest()
+                    .body("Admin user not authenticated");
+        }
+        logger.info("Creating districts from Census v1 data");
+        try {
+            int createdCount = geoService.createDistricts();
+            return ResponseEntity.ok().body("Created " + createdCount + " new districts");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest()
+                    .body("Could not create districts");
+        }
+    }
 
+    /**
+     * Creates villages based on the contents of census_full.csv
+     * in the resources/static directory
+     **/
+    @PostMapping("villages/{auth}")
+    public ResponseEntity<?> createVillages(@PathVariable String auth) {
+        if (!auth.equals(PASSWORD)) {
+            return ResponseEntity.badRequest()
+                    .body("Admin user not authenticated");
+        }
+        logger.info("Creating villages from Census v1 data");
+        try {
+            int createdCount = geoService.createVillages();
+            return ResponseEntity.ok().body("Created " + createdCount + " new villages");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest()
+                    .body("Could not create villages");
+        }
+    }
+
+    /**
+     * Creates subvillages based on the contents of census_full.csv
+     * in the resources/static directory
+     **/
+    @PostMapping("subvillages/{auth}")
+    public ResponseEntity<?> createsubvillages(@PathVariable String auth) {
+        if (!auth.equals(PASSWORD)) {
+            return ResponseEntity.badRequest()
+                    .body("Admin user not authenticated");
+        }
+        logger.info("Creating subvillages from Census v1 data");
+        try {
+            int createdCount = geoService.createSubvillages();
+            return ResponseEntity.ok().body("Created " + createdCount + " new subvillages");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest()
+                    .body("Could not create subvillages");
+        }
+    }
 }
--- src/main/java/com/ekichabi_business_registration/db/repository/VillageRepository.java
+++ src/main/java/com/ekichabi_business_registration/db/repository/VillageRepository.java
@@ -1,9 +1,13 @@
 package com.ekichabi_business_registration.db.repository;
 
+import com.ekichabi_business_registration.db.entity.DistrictEntity;
 import com.ekichabi_business_registration.db.entity.VillageEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VillageRepository extends CrudRepository<VillageEntity, Integer> {
+    boolean existsByNameAndDistrict(String name, DistrictEntity district);
+
+    VillageEntity findByNameAndDistrict(String village, DistrictEntity parentDistrict);
 }
