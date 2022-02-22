d339c58732fc50a9c49f8aa7642cb5030f74e753
Luke LeVasseur <llevasseur@flexport.com>
2022-02-22 08:32:42 -0800

    Temp

--- src/main/java/com/ekichabi_business_registration/db/entity/PhoneNumberEntity.java
+++ src/main/java/com/ekichabi_business_registration/db/entity/PhoneNumberEntity.java
@@ -1,7 +1,8 @@
 package com.ekichabi_business_registration.db.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -11,7 +12,7 @@ import javax.persistence.Table;
 @Table(name = "phone_number_entity")
 public class PhoneNumberEntity {
     @Id
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @ManyToOne
--- src/main/resources/db/model.sql
+++ src/main/resources/db/model.sql
@@ -65,3 +65,14 @@ CREATE TABLE subvillage
         CONSTRAINT subvillage_village_fk
             REFERENCES village (id)
 );
+
+CREATE TABLE phone_numbers
+(
+    id   SERIAL NOT NULL
+        CONSTRAINT phone_number_pk
+            PRIMARY KEY,
+    number TEXT
+);
+
+ALTER TABLE business
+    ADD COLUMN phone_number_id INT REFERENCES phone_numbers (id);
