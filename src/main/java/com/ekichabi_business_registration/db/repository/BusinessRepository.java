a3ae5a01805f057b58b6b311191fe0919962e611
Sonia Fereidooni <sonia.rezaie@gmail.com>
2022-02-24 03:02:50 -0800

    Impl get business endpoints

--- src/main/java/com/ekichabi_business_registration/db/repository/BusinessRepository.java
+++ src/main/java/com/ekichabi_business_registration/db/repository/BusinessRepository.java
@@ -2,6 +2,9 @@ package com.ekichabi_business_registration.db.repository;
 
 import com.ekichabi_business_registration.db.entity.BusinessEntity;
 import com.ekichabi_business_registration.db.entity.CategoryEntity;
+import com.ekichabi_business_registration.db.entity.DistrictEntity;
+import com.ekichabi_business_registration.db.entity.SubcategoryEntity;
+import com.ekichabi_business_registration.db.entity.VillageEntity;
 import com.ekichabi_business_registration.db.entity.SubvillageEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -13,4 +16,7 @@ public interface BusinessRepository extends CrudRepository<BusinessEntity, Long>
     boolean existsByCategoryAndSubvillage(CategoryEntity category,
                                           SubvillageEntity subvillage);
     List<BusinessEntity> findAllByCategory(CategoryEntity category);
+    List<BusinessEntity> findAllByDistrict(DistrictEntity district);
+    List<BusinessEntity> findAllBySubcategory(SubcategoryEntity subcategory);
+    List<BusinessEntity> findAllByVillage(VillageEntity village);
 }
